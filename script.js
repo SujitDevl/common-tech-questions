744a5d1460371e10cca5b7c77c1517454aff897b
SujitDevl <pandasujit687@gmail.com>
2025-02-04 21:00:03 +0530

    add new questions

--- script.js
+++ script.js
@@ -10,7 +10,26 @@
 function reverseString(str){
     return str.split('').reverse().join('');
 }
-console.log(reverseString('Hello'))
+// console.log(reverseString('Hello'))
 
 
 // ********* Check for Palindrome ***********
+
+function isPalindrome(word){
+    return word.split('').reverse().join('');
+}
+
+// console.log(isPalindrome("madam"))
+// console.log(isPalindrome("teacher"))
+
+
+// ************ Find the missing number in an array **********
+
+
+function findMissingNumber(arr, n){
+    const totalSum = (n *(n+1)) / 2;
+    const arrSum = arr.reduce((sum, num) => sum + num, 0 );
+    return totalSum -arrSum
+}
+
+console.log(findMissingNumber([1,2,3,5], 5))
