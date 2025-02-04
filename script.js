9fb8e6c779492f2b9d7e78d16dd81ab76e1a58f4
SujitDevl <pandasujit687@gmail.com>
2025-02-04 19:08:53 +0530

    common tech questions

---
+++ script.js
@@ -0,0 +1,17 @@
+// ********** Reverse a strinng ***************
+
+// function reverseString(str){
+//     return str.split('').reverse().join('');
+// }
+
+// console.log(reverseString('Sujit'))
+
+
+// function reverString(str){
+//     return str.split("").reverse().join('');
+// }
+
+// console.log(reverString('FAN'))
+
+
+// ********* Check for Palindrom ***********
