2b0719333355180db72eb94bc49df58b57452439
SujitDevl <pandasujit687@gmail.com>
2025-02-07 01:39:06 +0530

    new changes

--- script.js
+++ script.js
@@ -76,7 +76,8 @@ function findduplicate(nums) {
 
 function mergeSortedArrays(arr1, arr2) {
   let mergedArray = [];
-  let i = 0, j = 0;
+  let i = 0,
+    j = 0;
 
   while (i < arr1.length && j < arr2.length) {
     if (arr1[i] < arr2[j]) {
@@ -96,5 +97,4 @@ function mergeSortedArrays(arr1, arr2) {
   return mergedArray;
 }
 
-// console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); 
-
+// console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6]));
